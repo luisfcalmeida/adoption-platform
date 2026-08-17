5136899b1955389615705b6be9b8359434c35863
Luís de Almeida <luisfca@outlook.pt>
2026-08-17 12:34:49 +0100

    Initial commit

---
+++ src/main/java/com/luisdealmeida/adoptionplatform/AdoptionPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.luisdealmeida.adoptionplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptionPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoptionPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisdealmeida/adoptionplatform/AdoptionPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisdealmeida.adoptionplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptionPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
